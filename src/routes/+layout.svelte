f0f798839f190e912108bf883a09a9e808c08a8b
unixfy <alex@unixfy.net>
2023-06-18 19:30:09 -0400

    Bootstrap basic page interface

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,11 +20,10 @@
             }).addTo(map);
 
             let exits = await data.streamed.exits
-            console.log(exits)
 
             // add each exit to the map
             exits.forEach(exit => {
-                leaflet.circleMarker([exit.lat, exit.lon]).addTo(map);
+                const marker = leaflet.circleMarker([exit.lat, exit.lon]).addTo(map).bindPopup('blah');
             })
 
         }
@@ -40,7 +39,35 @@
 <style>
     @import 'leaflet/dist/leaflet.css';
 </style>
-<div class="h-screen w-screen">
-    <div bind:this={mapElement} class="h-full w-full"></div>
+
+<div class="h-screen w-screen flex flex-col lg:flex-row">
+    <!--    Page content -->
+    <div class="p-4 lg:p-8 lg:w-1/3">
+        <!--        Show loading if loading -->
+        {#await data.streamed.exits}
+            <div class="alert alert-info mb-4">
+                <span class="loading loading-dots loading-md"></span>
+                <p>
+                    <b>Please wait</b> while we load the data.
+                </p>
+            </div>
+        {:catch error}
+            <div class="alert alert-error mb-4">
+                <p>An error has occurred: {error}</p>
+            </div>
+        {/await}
+
+        <!--    Slot -->
+        <div>
+            <slot/>
+        </div>
+        <!--        Footer -->
+        <hr class="my-4">
+        <p class="italic">Copyright &copy; {new Date().getFullYear()} Unixfy. We run on SvelteKit and OpenStreetMaps.</p>
+    </div>
+    <!--    Map -->
+    <div class="lg:w-2/3 h-full w-full">
+        <div bind:this={mapElement} class="h-full w-full"></div>
+    </div>
 </div>
-<slot/>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>This is <b>ISCMM</b></h1>
+<h2>The Interactive SoCal Mobility Map</h2>
+
+<p class="mt-4">
+    This is a website that helps you learn about mobility options in Southern California.
+</p>
