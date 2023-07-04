48837041843ec2daf065723adacbf5a1c99bebf8
unixfy <alex@unixfy.net>
2023-07-03 21:29:31 -0400

    Add border to routebullet if no color

--- src/lib/RouteBullet.svelte
+++ src/lib/RouteBullet.svelte
@@ -3,5 +3,5 @@
     export let background;
 </script>
 
-<div class="px-4 py-4 rounded-full font-bold leading-4"
+<div class="px-4 py-4 rounded-full font-bold leading-4 border-base-100" class:border={!background}
       style="background-color: {background}">{ref}</div>
