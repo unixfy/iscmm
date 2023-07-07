1469298361a2479bfe61c3d6690842ab6c4573c4
unixfy <alex@unixfy.net>
2023-07-06 21:53:27 -0400

    Tweak spacing/padding on route bullets

--- src/lib/RouteBullet.svelte
+++ src/lib/RouteBullet.svelte
@@ -3,5 +3,7 @@
     export let background;
 </script>
 
-<div class="px-4 py-4 rounded-full font-bold leading-4 border-base-100" class:border={!background}
-      style="background-color: {background}">{ref}</div>
+<div class="px-4 py-2 rounded-full font-bold border-base-100" class:border={!background}
+      style="background-color: {background}">
+    <p class="leading-0">{ref}</p>
+</div>
