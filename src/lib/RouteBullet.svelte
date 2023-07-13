5e768e72d69d9c8a3b99b5170b488e2cb4efe3eb
unixfy <alex@unixfy.net>
2023-07-13 11:01:16 -0400

    Apparently the Overpass font has some weird height issues that mess up the route bullet

--- src/lib/RouteBullet.svelte
+++ src/lib/RouteBullet.svelte
@@ -3,7 +3,7 @@
     export let background;
 </script>
 
-<div class="px-4 py-2 rounded-full font-bold border-base-100" class:border={!background}
+<div class="px-4 py-2 text-xl rounded-full font-bold border-base-100 font-sans" class:border={!background}
       style="background-color: {background}">
-    <p class="leading-0">{ref}</p>
+    <p class="leading-8">{ref}</p>
 </div>
